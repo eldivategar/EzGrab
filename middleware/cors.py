6992f07865f7c37c5cdad7fe5407a785b8471f01
eldivategar <tegareldiva@students.amikom.ac.id>
2024-08-23 16:16:00 +0700

    cors origin

--- middleware/cors.py
+++ middleware/cors.py
@@ -10,6 +10,6 @@ def add_cors_middleware(app):
         allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"],
+        allow_headers=["*", "X-Requested-With", "Content-Type", "Access-Control-Allow-Origin"],
         expose_headers=["Content-Disposition"],
     )
